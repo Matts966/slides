b63b2d83891ecaf097ea62afe83d80e9ef50968d
Matts966 <Matts966@users.noreply.github.com>
2020-05-12 09:30:30 +0900

    Use root libs to decrease repo size

--- preprocess.sh
+++ preprocess.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+
+file=$1
+if [[ ! -f $file ]] || [[ $# -ne 1 ]]; then
+    echo "Please specify only 1 path to html."
+    exit 1
+fi
+
+# Use common libs on root.
+rm -rf $(dirname $1)/libs
+sed -i '' "s/\"libs\/reveal\.js/\"\.\/libs\/reveal\.js/g" $1
+sed -i '' "s/\'libs\/reveal\.js/\'\.\/libs\/reveal\.js/g" $1
+
+# Render math.
+tmp=$(mktemp)
 ./node_modules/mathjax-node-page/bin/mjpage \
     --output CommonHTML --dollars true \
-    < $1
+    < $1 > $tmp
+cat $tmp > $1
