55f756d83df72196ae32b40921365f8c33d46abe
Matts966 <Matts966@users.noreply.github.com>
2020-05-12 20:55:50 +0900

    Use /slides url

--- preprocess.sh
+++ preprocess.sh
@@ -8,7 +8,7 @@ fi
 
 # Use common libs on root.
 rm -rf $(dirname $1)/libs
-sed -i '' "s/\"libs\/reveal\.js/\"\.\/libs\/reveal\.js/g" $1
+sed -i '' "s/\"libs\/reveal\.js/\"/slides/libs\/reveal\.js/g" $1
 sed -i '' "s/\'libs\/reveal\.js/\'\.\/libs\/reveal\.js/g" $1
 
 # Render math.
